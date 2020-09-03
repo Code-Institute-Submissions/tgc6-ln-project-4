d2bc8457780f3019a45c1acb0027d715e80fb5ff
LionelNg-CI <lionel.ng.y.c@gmail.com>
2020-09-03 12:16:35 +0000

    tested successful login user page

--- faceMaskShop/urls.py
+++ faceMaskShop/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import checkout.views, products.views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('checkout/', checkout.views.index),
     path('', products.views.index),
     # path('checkout/', checkout.views.checkout)
