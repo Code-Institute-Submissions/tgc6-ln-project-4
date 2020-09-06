73612d672cdd8b52d5c69ab242c801e954e1ed5e
LionelNg-CI <lionel.ng.y.c@gmail.com>
2020-09-06 10:39:20 +0000

    working and test on product model

--- products/models.py
+++ products/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Product(models.Model):
     name = models.CharField(blank=False, max_length=80)
-    selling_price = models.DecimalField(blank=False, max_digits=4, decimal_places=2)
+    selling_price = models.DecimalField(blank=False,
+                                        max_digits=4,
+                                        decimal_places=2)
     quantity = models.IntegerField(blank=False)
 
     def __str__(self):
-        return self.title
+        return self.name
