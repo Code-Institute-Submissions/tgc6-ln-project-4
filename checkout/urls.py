a98f37601631f99fe546b32e8cb8ba360ad1b660
LionelNg-CI <lionel.ng.y.c@gmail.com>
2020-10-06 07:29:55 +0000

    Complete basic project requirement backend

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 import checkout.views
 
 urlpatterns = [
-    path('', checkout.views.checkout, name='checkout'),
-    path('success/', checkout.views.success, name='success'),
-    path('cancelled/', checkout.views.cancelled, name='cancelled'),
-    path('completed/', checkout.views.completed, name='completed'),
-    path('charge/', checkout.views.charge, name='charge'),
+    path('', checkout.views.charge, name='charge'),
+    # path('success/', checkout.views.success, name='success'),
+    # path('cancelled/', checkout.views.cancelled, name='cancelled'),
+    # path('completed/', checkout.views.completed, name='completed'),
+    # path('charge/', checkout.views.charge, name='charge'),
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -12,52 +12,13 @@ from django.contrib.sites.models import Site
 # Create your views here.
 
 
-def checkout(request):
+def charge(request):
     stripe.api_key = settings.STRIPE_SECRET_KEY
     cart = request.session.get('shopping_cart', {})
 
-    return render(request, 'checkout/checkout.template.html')
 
-
-def success(request):
-
-    return render(request, 'checkout/success.template.html')
-
-
-def cancelled(request):
-
-    return render(request, 'checkout/cancelled.template.html')
-
-
-@csrf_exempt
-def completed(request):
-    payload = request.body
-    sig_header = request.META["HTTP_STRIPE_SIGNATURE"]
-    event = None
-
-    try:
-        event = stripe.Webhook.construct_event(
-        payload, sig_header, endpoint_secret
-             )
-    except ValueError as e:
-        # Invalid payload
-        return HttpResponse(status=400)
-    except stripe.error.SignatureVerificationError as e:
-        # Invalid signature
-        return HttpResponse(status=400)
-        # Handle the checkout.session.completed event
-    if event['type'] == 'checkout.session.completed':
-        session = event['data']['object']
-        # Fulfill the purchase...
-        handle_payment(session)
-    return HttpResponse(status=200)
-
-
-def charge(request):
     if request.method == 'GET':
         amount = 100
-        # or
-        # request.GET['amount']
         key = settings.STRIPE_PUBLISHABLE_KEY  # 1
         return render(request, 'checkout/charge.template.html', {
         'key': key,
